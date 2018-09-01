4e907db02b00a0e95e641fd7bf020bccea936af8
ericabruenton <ercfit@gmail.com>
2018-09-01 17:02:40 -0600

    cleaned up sytax throwing warnings

--- tennislog/src/components/tennisLog/Log.js
+++ tennislog/src/components/tennisLog/Log.js
@@ -23,63 +23,63 @@ class Log extends Component {
         return (
             <div>
                 <form>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="location">Session Location</label>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="location">Session Location</label>
                             <input
                                 type="text"
                                 name="location"
-                                class="form-control"
+                                className="form-control"
                                 id="location"
                                 placeholder="Session Location" />
                         </div>
-                        <div class="form-group col-md-6">
-                            <label for="date">Session Date</label>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="date">Session Date</label>
                             <input
                                 type="date"
                                 name="date"
-                                class="form-control"
+                                className="form-control"
                                 id="date"
                                 placeholder="Date" />
                         </div>
                     </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="sessionType">Session Type</label>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="sessionType">Session Type</label>
                             <input
                                 type="text"
                                 name="sessionType"
-                                class="form-control"
+                                className="form-control"
                                 id="sessionType"
                                 placeholder="Session Type" />
                         </div>
-                        <div class="form-group col-md-6">
-                            <label for="date">Coach</label>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="date">Coach</label>
                             <input
                                 type="text"
                                 name="coach"
-                                class="form-control"
+                                className="form-control"
                                 id="coach"
                                 placeholder="Coach" />
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="participants">Other Participants</label>
-                        <input type="text" class="form-control" id="participants" placeholder="Doubles Partner" />
+                    <div className="form-group">
+                        <label htmlFor="participants">Other Participants</label>
+                        <input type="text" className="form-control" id="participants" placeholder="Doubles Partner" />
                     </div>
-                    <div class="form-group">
-                        <label for="sessionFocus">Session Focus</label>
-                        <textarea class="form-control" id="sessionFocus" rows="3"></textarea>
+                    <div className="form-group">
+                        <label htmlFor="sessionFocus">Session Focus</label>
+                        <textarea className="form-control" id="sessionFocus" rows="3"></textarea>
                     </div>
-                    <div class="form-group">
-                        <label for="positiveTakeAways">Positive Take Aways</label>
-                        <textarea class="form-control" id="positiveTakeAways" rows="6"></textarea>
+                    <div className="form-group">
+                        <label htmlFor="positiveTakeAways">Positive Take Aways</label>
+                        <textarea className="form-control" id="positiveTakeAways" rows="6"></textarea>
                     </div>
-                    <div class="form-group">
-                        <label for="areasOfOpportunity">Areas of Opportunity</label>
-                        <textarea class="form-control" id="areasOfOpportunity" rows="6"></textarea>
+                    <div className="form-group">
+                        <label htmlFor="areasOfOpportunity">Areas of Opportunity</label>
+                        <textarea className="form-control" id="areasOfOpportunity" rows="6"></textarea>
                     </div>
-                    <button type="submit" class="btn btn-primary">Add Log</button>
+                    <button type="submit" className="btn btn-primary">Add Log</button>
                 </form>
             </div>
         );
